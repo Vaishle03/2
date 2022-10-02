bd6a91ac3cd8851108187030722874ca7d8e99dc
Vaishle03 <v.vaishle03@ukr.net>
2022-10-02 16:05:57 +0300

    lab4

--- web/project.js
+++ web/project.js
@@ -1,84 +1,309 @@
-console.log("#   #   #   #   ");
-console.log("  #   #   #   #");
-console.log("#   #   #   #   ");
-console.log("  #   #   #   #");
-console.log("#   #   #   #   ");
-console.log("  #   #   #   #");
-console.log("#   #   #   #   ");
-console.log("  #   #   #   #");
+console.log('1')
 
+let a = 1, b = 0, c = -3;
 
-console.log('\n\n');
-
-
-for (let i = 1; i <= 100; i++) { 
-    if(i%3==0)
-    console.log("lol where is number?")
-    else
-    console.log(i)
+  if(a == 0){
+    console.log("Вірно")
   }
+  else
+    console.log("Невірно")
+
+  if(b == 0){
+    console.log("Вірно")
+  }
+ else
+    console.log("Невірно")
+
+  if(c == 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n2\n')
 
 
-  console.log('\n\n');
+  if(a > 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(b > 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(c > 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n3\n')
 
 
-  let a = "" + 1 + 0;//string type
-  console.log(a);
+  if(a < 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
 
-  let b = "" - 1 + 0;//number type
-  console.log(b);
+  if(b < 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
 
-  let c = true + false;//number type
-  console.log(c);
+  if(c < 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n4\n')
 
-  let d = 6 / "3";//number type
-  console.log(d);
 
-  let e = "2"*"3";//number type
-  console.log(e);
+  if(a >= 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
 
-  let f = 4 + 5 + "px";//string type
-  console.log(f);
+  if(b >= 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
 
-  let g = "$ + 4 + 5";//string type
-  console.log(g);
+  if(c >= 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n5\n')
 
-  let h = "4" - 2;//number type
-  console.log(h);
 
-  let i = "4px" - 2;//number type
-  console.log(i);
+  if(a <= 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
 
-  let j = 7 / 0;//number type
-  console.log(j);
+  if(b <= 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
 
-  let k = "-9\n" + 5;//string type
-  console.log(k);
+  if(c <= 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n6\n')
 
-  let l = "-9\n" - 5;//number type
-  console.log(l);
 
-  let m = 5 && 2;//number type
-  console.log(m);
+  if(a != 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
 
-  let n = 2 && 5;//number type
-  console.log(n);
+  if(b != 0){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
 
-  let o = 5||0;//number type
-  console.log(o);
+  if(c != 0){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n7\n')
 
-  let p = 0||5;//number type
-  console.log(p);
 
-  let q = null + 1 ;//number type
-  console.log(q);
+ let a1 = 'test', b1 = 'тест', c1 = 3;
 
-  let r = undefined + 1;//number type
-  console.log(r);
+  if(a1 === 'test'){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
 
-  let s = null == "\n0\n";//boolean type
-  console.log(s);
+  if(b1 ==='test'){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
 
-  let t = null == +"\n0\n";//boolean type
-  console.log(t);
-  
+  if(c1 === 'test'){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n8\n')
+
+
+ let a2 = '1', b2 = 1, c2 = 3;
+
+  if(a2 === '1' && (typeof a2) ===(typeof '1')){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(b2 === '1' && (typeof b2) ===(typeof '1')){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(c2 === '1' && (typeof c2) ===(typeof '1')){
+      console.log("Вірно")
+    }
+  else
+      console.log("Невірно")
+ console.log('\n9\n')
+
+
+ let test1 = true, test2 = false;
+
+  if(test1 == true ){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+
+  if(test2 == true){
+    console.log("Вірно")
+  }
+  else
+    console.log("Невірно")
+ console.log('\n10\n')
+
+
+ if(test1 != true ){
+  console.log("Вірно")
+}
+else
+  console.log("Невірно")
+
+if(test2 != true){
+  console.log("Вірно")
+}
+else
+  console.log("Невірно")
+console.log('\n11\n')
+
+let a3 = 5, b3 = 0, c3 = -3, d3 = 2;
+
+if(a3 > 0 && a3 < 5){
+    console.log("Вірно")
+  }
+else
+    console.log("Невірно")
+
+if(b3 > 0 && b3 < 5){
+  console.log("Вірно")
+    }
+else
+ console.log("Невірно")
+
+if(c3 > 0 && c3 < 5){
+  console.log("Вірно")
+      }
+else
+  console.log("Невірно")
+if(d3 > 0 && d3 < 5){
+  console.log("Вірно")
+        }
+else
+  console.log("Невірно")
+ console.log('\n12\n')
+
+
+
+if(a3 == 0 || a3 == 2){
+  console.log(a3+7)
+}
+else
+  console.log(a3/10)
+
+if(b3 == 0 || b3 == 2){
+  console.log(b3+7)
+  }
+else
+  console.log(b3/10)
+
+if(c3 == 0 || c3 == 2){
+  console.log(c3+7)
+    }
+else
+  console.log(c3/10)
+
+if(d3 == 0 || d3 == 2){
+  console.log(d3+7)
+      }
+else
+  console.log(d3/10)
+console.log('\n13\n')
+
+
+let a4 = 1, b4 = 3;
+let a5 = 0, b5 = 6;
+let a6 = 3, b6 = 5;
+
+if(a4 <=1 && b4 >=3)
+ console.log(a4+b4)
+else 
+ console.log(a4-b4)
+
+if(a5 <= 1 && b5 >= 3)
+ console.log(a+b)
+else 
+ console.log(a5-b5)
+
+if(a6 <=1 && b6 >=3)
+ console.log(a6+b6)
+else 
+ console.log(a6-b6)
+console.log('\n14\n')
+
+
+if((a4 > 2 && a4 < 11)||(b4 >= 6 && b4 < 14))
+ console.log('Вірно')
+else 
+ console.log('Невірно')
+
+ if((a5 > 2 && a5 < 11)||(b5 >= 6 && b5 < 14))
+ console.log('Вірно')
+else 
+ console.log('Невірно')
+
+ if((a6 > 2 && a6 < 11)||(b6 >= 6 && b6 < 14))
+ console.log('Вірно')
+else 
+ console.log('Невірно')
+console.log('\n15\n')
+
+
+let num = 4, result
+
+switch (num) {
+  case 1:
+    result = 'Зима'
+    console.log( result );
+     break
+     case 2:
+      result = 'Весна'
+      console.log( result );
+       break
+       case 3:
+        result = 'Літо'
+        console.log( result );
+         break
+         case 4:
+          result = 'Осінь'
+           console.log( result );
+           break
+}
